08c4f914ecfc16f55a7c49aebb6f52e799f8bfb8
Kevin Gin <kevinagin@gmail.com>
2017-02-08 19:00:30 -0800

    (feature) implement topic search on db, server, and request on client

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -7,13 +7,15 @@ var {User} = require('../db/index.js');
 
 
 var getTweetsAsync = Promise.promisify(twitterUtil.getTweets, {context: twitterUtil, multiArgs: true});
+//KG Added next:
+var getTweetsByTopicAsync = Promise.promisify(twitterUtil.getTweetsByTopic, {context: twitterUtil, multiArgs: true});
 var getSentimentAsync = Promise.promisify(havenUtil.getSentiment, {context: havenUtil});
 
 module.exports = {
-  getAnalysis: function(req, res, next) {
+  getUserAnalysis: function(req, res, next) {
     console.log('getAnalysis CALLED');
 
-    // Using hardcoded twitter handle for testing purposes, default currently pulls 5 most recent tweets
+    // reads twitterHandle off query string
     var twitterHandle = req.query.handle //|| 'defaultTwitterHandle';
     var location = req.query.location //|| 'defaultLocation';
     var topic = req.query.topic //|| 'defaultTopic';
@@ -40,17 +42,76 @@ module.exports = {
     })
     .then(function(user) {
       console.log('CREATING SCORE');
-      console.log(user, '---------------------');
+      // console.log(user, '---------------------');
       // Work here
       return Score.create({
         twitterHandle: twitterHandle,
-        location: location,
-        topic: topic,
+        location: location, //should be undefined if only testing handle
+        topic: topic, //should be undefined if only testing handle
         numTweets: globaldata.length,
         tweetText: globaltweetData.string,
         sentimentScore: globalsentiment,
         retweetCount: globaltweetData.retweetCount,
         favoriteCount: globaltweetData.favoriteCount
+      })
+        .then((newScore) => newScore.setUser(user.id)
+        .then((newScore) => newScore));
+    })
+    .then((newScore) => {
+      console.log('New score created!');
+      return res.status(200).json(newScore);
+    })
+    .catch((err) => {
+      console.error('Analysis error ', err);
+      return res.status(404).end();
+    });
+  },
+
+
+  getTopicAnalysis: function(req, res, next) {
+    console.log('getTopicAnalysis CALLED');
+
+    // reads topic off query string
+    var twitterHandle = req.query.handle //|| 'defaultTwitterHandle';
+    var location = req.query.location //|| 'defaultLocation';
+    var topic = req.query.topic //|| 'defaultTopic';
+
+    console.log('topic ===>', topic);
+
+    var currentUser = req.params.user || 'RipplMaster';
+    var globaldata, globaltweetData, globalsentiment, globaluser;
+
+    console.log('KG: About to do API request')
+    
+    getTweetsByTopicAsync(topic)
+    .spread((data, response) => {
+      console.log('inside .spread of getTweetsByTopicAsync in controller');
+      // console.log('data ==> ', data);
+      // console.log('response ==> ', response);
+      globaldata = data;
+      // console.log(data.statuses);
+      // console.log(data.statuses.length);
+
+      //KG WORKING FROM HERE
+      globalTweetString = twitterUtil.getTweetStringForTopic(globaldata);
+      console.log('tweetString ==> ', globalTweetString)
+      // globaltweetData = twitterUtil.getTweetString(globaldata);
+
+      console.log('finished getTweetString')
+      // Need to look into handling haven asynchronously 
+      return getSentimentAsync(twitterHandle, globalTweetString);
+    })
+    .then((sentiment) => {
+      globalsentiment = sentiment;
+      console.log('response ==>', sentiment);
+      return User.findOne({username: currentUser});
+    })
+    .then(function(user) {
+      console.log('CREATING SCORE');
+      return Score.create({
+        topic: topic,
+        tweetText: globalTweetString,
+        sentimentScore: globalsentiment,
       })
         .then((newScore) => newScore.setUser(user.id)
         .then((newScore) => newScore));
