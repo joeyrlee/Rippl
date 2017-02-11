6e6703cbec6d7f158d2a2e0e0e657372f8b6eaa4
Joey Lee <jleepianist@gmail.com>
2017-02-10 19:54:23 -0800

    (Feature Backlog) Added a pending bugfux request to Feature Backlog.md

--- public/components/StatsBox.jsx
+++ public/components/StatsBox.jsx
@@ -15,7 +15,7 @@ class StatsCard extends React.Component{
   }
 
   render(){
-    // console.log("StatsBox");
+    console.log("StatsBox; this.props.user: ",this.props.user);
   	return (
       <div className="statsbox">
         <Row>
